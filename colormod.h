9871fef251c7aad87db6188dec9263eb8875a29d
Linux_Hat <adrien@d4x.fr>
2024-05-12 12:41:01 +0200

    more colors

--- colormod.h
+++ colormod.h
@@ -1,23 +1,36 @@
 #pragma once
+
 #include <ostream>
+
 namespace Color {
     enum Code {
+        FG_BLACK    = 30,
         FG_RED      = 31,
         FG_GREEN    = 32,
+        FG_YELLOW   = 33,
         FG_BLUE     = 34,
+        FG_MAGENTA  = 35,
+        FG_CYAN     = 36,
+        FG_WHITE    = 37,
         FG_DEFAULT  = 39,
+        BG_BLACK    = 40,
         BG_RED      = 41,
         BG_GREEN    = 42,
+        BG_YELLOW   = 43,
         BG_BLUE     = 44,
+        BG_MAGENTA  = 45,
+        BG_CYAN     = 46, 
+        BG_WHITE    = 47,
         BG_DEFAULT  = 49
     };
+
     class Modifier {
         Code code;
-    public:
-        Modifier(Code pCode) : code(pCode) {}
-        friend std::ostream&
-        operator<<(std::ostream& os, const Modifier& mod) {
-            return os << "\033[" << mod.code << "m";
-        }
+        public:
+            Modifier(Code pCode) : code(pCode) {}
+            friend std::ostream&
+            operator<<(std::ostream& os, const Modifier& mod) {
+                return os << "\033[" << mod.code << "m";
+            }
     };
 }
